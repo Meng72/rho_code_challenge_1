7887cca3e2e9be84529462d75061b4a9cb80a363
Meng72 <matthewkeng@hotmail.com>
2016-09-26 15:52:20 -0500

    initial commit

--- 2-loops/all_your_loops.js
+++ 2-loops/all_your_loops.js
@@ -3,8 +3,15 @@
     value of checkz by 1.
 */
 var checkz = 0;
-
+for (i=0; i<5; i++) {
+  checkz+=1;
+  return checkz;
+}
 /*
 2 - Write a while loop that runs 3 times. Each iteration will decrease
     the value of checkz by 2
 */
+var i=0;
+while (i<3){
+  checkz-=2; i++;
+};
--- 4-arrays/builtins.js
+++ 4-arrays/builtins.js
@@ -1,9 +1,9 @@
 // 1 - Add the value 32 to the end of the following array.
 
 var arr = [3, 4, 6, 0];
-
+arr.push(32);
 // 2 - Remove the values 4 & 6 from arr using the splice method.
-
+arr.splice(1,2);
 // 3 - Use the sort method to order each array value from smallest to largest.
 
 /*
