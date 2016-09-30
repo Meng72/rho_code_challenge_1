63c1c4eda30cfbcf2d4962b3a4857c5ab8e6bf11
Meng72 <matthewkeng@hotmail.com>
2016-09-30 10:58:54 -0500

    finished challenge

--- 5-functions/func.js
+++ 5-functions/func.js
@@ -1,15 +1,22 @@
 // 1 - Write a function declaration that takes no arguments and returns the value 4
-
+function four() {
+  return 4;
+}
 /*
 2 - Write a function declaration that takes two arguments and returns the value
     of the result of the multiplication of both arguments
-*/
+*/function multiplier(a,b) {
+  return a * b;
+}
 
 // 3 - Write a function expression that takes one argument and console logs that value.
-
+var x = function (a) {console.log(a)}
 // 4 - Write a function declaration that takes a number and divides it by two.
-
+function halver(x) {
+  return x/2;
+}
 /*
 5 - Call the function from 4 and pass it into the function from 2 called with
     the arguments 10, 20. Console log the result.
 */
+console.log(halver(multiplier(10,20)));
